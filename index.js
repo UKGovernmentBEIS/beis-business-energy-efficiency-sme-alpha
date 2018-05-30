3b608fe341df914f514151238ae9b0b60742aaeb
Jamie Humphries <jamie.humphries@softwire.com>
2018-05-30 11:39:27 +0100

    Set up ESLint with Standard JS rules.

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const express = require("express");
-const mustacheExpress = require("mustache-express");
-const bodyParser = require("body-parser");
+const express = require('express')
+const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
+const path = require('path')
 
-const app = express();
+const app = express()
 
 // Register '.mustache' extension with The Mustache Express
-app.engine('html', mustacheExpress());
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+app.engine('html', mustacheExpress())
+app.set('view engine', 'html')
+app.set('views', path.join(__dirname, '/views'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.render('index.html', {
-    "name": "Jamie"
-  });
-});
+  res.render('index.html')
+})
 
 app.post('/search', (req, res) => {
   res.render('results.html', {
-    "postcode": req.body.postcode
+    'postcode': req.body.postcode
   })
 })
 
-const port = 5000;
+const port = 5000
 
-app.listen(port, () => console.log(`Listening on port ${port}…`));
+app.listen(port, () => console.log(`Listening on port ${port}.`))
