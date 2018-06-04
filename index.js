9e31ecd48eb111091eb92fc2eb51bf7f5c767ced
Jamie Humphries <jamie.humphries@softwire.com>
2018-06-04 17:12:12 +0100

    Reorder and relabel recommendation table.

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get('/search', (req, res) => {
 })
 
 const PAYBACK_TYPE_ORDER = { 'SHORT': 0, 'MEDIUM': 1, 'LONG': 2, 'OTHER': 3 }
+const PAYBACK_TYPE_TITLES = { 'SHORT': '0-2 years', 'MEDIUM': '3-6 years', 'LONG': '7+ years' }
 app.get('/rating/:certificateHash', (req, res) => {
   const { certificateHash } = req.params
   odcApiClient.getCertificate(certificateHash).then(({ property, recommendations }) => {
@@ -38,6 +39,7 @@ app.get('/rating/:certificateHash', (req, res) => {
     recommendations.forEach(recommendation => {
       recommendation.paybackTypeOrder = PAYBACK_TYPE_ORDER[recommendation.PAYBACK_TYPE]
       recommendation.paybackTypeColor = ratingColorHelper.getRecommendationColor(recommendation.paybackTypeOrder)
+      recommendation.paybackTypeTitle = PAYBACK_TYPE_TITLES[recommendation.PAYBACK_TYPE] || '-'
     })
     recommendations = _.sortBy(recommendations, ['paybackTypeOrder'])
     res.render('rating', { certificateHash, property, recommendations })
